bd9bd97a9a9792eaba7a2ee97523e8363d8f8e0c
Gyol <ryunhwa.kang@gmail.com>
2020-10-17 01:38:22 +0900

    do not put number on the title

--- 02. Python Web Scraper/03-2. main.py
+++ 02. Python Web Scraper/03-2. main.py
@@ -1,4 +1,7 @@
-from indeed import extract_indeed_pages
+from indeed import extract_indeed_pages, extract_indeed_jobs
 
-max_indeed_pages = extract_indeed_pages
+last_indeed_page = extract_indeed_pages()
 
+print(last_indeed_page)
+
+extract_indeed_jobs(last_indeed_page)
--- 02. Python Web Scraper/indeed.py
+++ 02. Python Web Scraper/indeed.py
@@ -1,21 +1,29 @@
 import requests as req
 from bs4 import BeautifulSoup as bs
 
-INDEED_URL = "https://kr.indeed.com/jobs?q=python&l=seoul&sort=date"
+
+START = 10
+URL = f"https://kr.indeed.com/jobs?q=python&l=seoul&sort=date"
 
 
 def extract_indeed_pages():
-    result = req.get(INDEED_URL)
+    result = req.get(URL)
 
-    soup = bs(result.test, "html.parser")
+    soup = bs(result.text, "html.parser")
 
     pagination = soup.find("div", {"class": "pagination"})
 
     links = pagination.find_all('a')
     pages = []
-    for link in links[:1]:
+    for link in links[:-1]:
         pages.append((int(link.string)))
 
     max_page = pages[-1]
 
     return max_page
+
+
+def extract_indeed_jobs(last_page):
+    for page in range(last_page):
+        result = req.get(f"{URL}&start={page*START}")
+        print(result.status_code)
